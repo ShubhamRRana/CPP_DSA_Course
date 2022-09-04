e900960722ffca27bbb6a8fedb44f5bddc85e80b
Shubham Rana <shubham.r0098@gmail.com>
2022-09-04 15:11:16 +0530

    Seen few examples

---
+++ example.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=3;
+    // cout<<"Enter n:";
+    // cin>>n;
+    // cout<<"n="<<n<<endl;
+    cout << n << endl;
+    char a='a';
+    cout<<a<<endl;
+    cout<<"Hello World!!!"<<endl;
+}
